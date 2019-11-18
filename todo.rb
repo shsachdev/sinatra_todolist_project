6eea7c16ea0475a1b7817cf1f196624d28f4671c
Shikhar Sachdev <shsachdev@gmail.com>
2019-11-17 19:49:50 -0800

    created flash message when list is successfully created

--- todo.rb
+++ todo.rb
@@ -15,16 +15,27 @@ get "/" do
   redirect "/lists"
 end
 
+# GET /lists => view all lists
+# GET /lists/new => new list form
+# POST /lists => create new list
+# GET /lists/1 => view a single list
+
+# resource type + ID of whatever the object is
+
+# View list of lists
 get "/lists" do
   @lists = session[:lists]
   erb :lists, layout: :layout
 end
 
+# Render the new list form
 get "/lists/new" do
   erb :new_list, layout: :layout
 end
 
+# Create a new list
 post "/lists" do
   session[:lists] << {name: params[:list_name], todos: []}
+  session[:success] = "The list has been created."
   redirect "/lists"
 end
