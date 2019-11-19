77d088ac727ea40a75d3d4d34760ccad0dcc00a2
Shikhar Sachdev <shsachdev@gmail.com>
2019-11-18 21:08:08 -0800

    created helper method error_for_list_name

--- todo.rb
+++ todo.rb
@@ -33,17 +33,27 @@ get "/lists/new" do
   erb :new_list, layout: :layout
 end
 
+# Return an error message if the name is invalid
+
+def error_for_list_name(name)
+  if !(1..100).cover? name.size
+    "List name must be between 1 and 100 characters."
+  elsif session[:lists].any? {|list| list[:name] == name}
+    "List name must be unique."
+  else
+    false
+  end
+end
+
 # Create a new list
 post "/lists" do
   list_name = params[:list_name].strip
-  if (1..100).cover? list_name.size
+  if error = error_for_list_name(list_name)
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
     session[:lists] << {name: list_name, todos: []}
     session[:success] = "The list has been created."
     redirect "/lists"
-  elsif session[:lists]
-
-  else
-    session[:error] = "List name must be between 1 and 100 characters."
-    erb :new_list, layout: :layout
   end
 end
