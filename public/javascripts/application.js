4a98aa0917afd2a8cc91806a29a1a9fbc368edbc
Shikhar Sachdev <shsachdev@gmail.com>
2019-11-25 22:23:58 -0800

    got rid of redirect if using ajax

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,7 +6,18 @@ $(function() {
 
     var ok = confirm("Are you sure? This cannot be undone!");
     if (ok) {
-      this.submit();
+      // this.submit();
+
+      var form = $(this);
+
+      var request = $.ajax({
+        url: form.attr("action"),
+        method:form.attr("method")
+      });
+
+      request.done(function(data, textStatus, jqHXR) {
+        form.parent("li").remove()
+      });
     }
   });
 
--- todo.rb
+++ todo.rb
@@ -190,8 +190,12 @@ post "/lists/:list_id/todos/:id/destroy" do
   todo_id = params[:id].to_i
 
   @list[:todos].delete_at(todo_id)
-  session[:success] = "The todo has been deleted."
-  redirect "/lists/#{@list_id}"
+  if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
+    status 204
+  else
+    session[:success] = "The todo has been deleted."
+    redirect "/lists/#{@list_id}"
+  end
 end
 
 # Update the status of a todos
