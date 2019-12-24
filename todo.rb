2f94027be235fb489611bc6e3de432620dd1c215
Shikhar Sachdev <shsachdev@gmail.com>
2019-12-24 11:03:09 +0100

    added database_persistence file

--- todo.rb
+++ todo.rb
@@ -3,6 +3,8 @@ require "sinatra/reloader" if development?
 require "tilt/erubis"
 require "sinatra/content_for"
 
+require_relative 'session_persistence'
+
 configure do
   enable :sessions
   set :session_secret, 'secret'
@@ -12,65 +14,6 @@ configure do
   set :erb, :escape_html => true
 end
 
-class SessionPersistence
-  def initialize(session)
-    @session = session
-    @session[:lists] ||= []
-  end
-
-  def find_list(id)
-    @session[:lists][id] if id && @session[:lists][id]
-  end
-
-  def all_lists
-    @session[:lists]
-  end
-
-  def create_new_list(list_name)
-    id = next_element_id(@session[:lists])
-    @session[:lists] << {id: id, name: list_name, todos: []}
-  end
-
-  def delete_list(id)
-    @session[:lists].delete_at(id)
-  end
-
-  def update_list_name(id, new_list_name)
-    list = find_list(id)
-    list[:name] = new_list_name
-  end
-
-  def create_new_todo(list_id, new_todo_name)
-    list = find_list(list_id)
-    id = next_element_id(list[:todos])
-    list[:todos] << {id: id, name: new_todo_name, completed: false}
-  end
-
-  def delete_todo_from_list(list_id, todo_id)
-    list = find_list(list_id)
-    list[:todos].reject! { |todo| todo[:id] == todo_id }
-  end
-
-  def update_todo_status(list_id, todo_id, is_completed)
-    list = find_list(list_id)
-    todo = list[:todos].find {|t| t[:id] == todo_id}
-    todo[:completed] = is_completed
-  end
-
-  def complete_all_todos(list_id)
-    list = find_list(list_id)
-    list[:todos].each do |todo|
-      todo[:completed] = true
-    end
-  end
-
-  private
-
-  def next_element_id(elements)
-    max = elements.map {|todo| todo[:id]}.max || 0
-    max + 1
-  end
-end
 
 def load_list(index)
   list = @storage.find_list(index)
